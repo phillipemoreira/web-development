6b6cbf0e7bfc16669d640251b8603c3087cd988e
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-02-26 19:25:56 -0300

    reorganizing

--- robpervival/4.jQuery/ajax/index.js
+++ robpervival/4.jQuery/ajax/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/client"));
+app.use(express.static(__dirname + "/bower_components"));
 app.listen(3000);
 
 console.log("AJAX app running in port 3000.");
