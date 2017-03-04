d2e178e6a9c1505acfa91f5e750582b701800c2a
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-03-04 16:27:00 -0300

    finishing code-player

--- robpervival/4.jQuery/server/client/js/codeplayer.js
+++ robpervival/4.jQuery/server/client/js/codeplayer.js
@@ -5,4 +5,28 @@ $('.codeplayer-code-container').css('height', (windowHeight - menuBarHeight) + '
 
 $('.codeplayer-toogle').click(function() {
 	$(this).toggleClass('codeplayer-selected');
+
+    var codeContainerDiv = '#codeplayer-' + $(this).html() + '-container';
+
+    $(codeContainerDiv).toggle();
+
+    var showingDivs = $('.codeplayer-code-container').filter(function() {
+        return $((this)).css('display') != 'none';
+    }).length;
+
+    var divWidthPercentage = 100 / showingDivs;
+
+    $('.codeplayer-code-container').css('width', divWidthPercentage + '%');
+
+});
+
+$('#codeplayer-runbuttondiv').click(function() {
+    var iframeContent = '<style>' +
+        $('#codeplayer-cssCode').val() +
+        '</style>' +
+        $('#codeplayer-htmlCode').val();
+
+    $('#codeplayer-iframe').contents().find('html').html(iframeContent);
+
+    document.getElementById('codeplayer-iframe').contentWindow. eval($('#codeplayer-jsCode').val())
 });
