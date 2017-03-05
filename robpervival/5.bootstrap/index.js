5944be05f5ff44ca4b324bd52a2ef1bca871b00f
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-03-05 15:51:24 -0300

    Bootstrap section infrastructure

---
+++ robpervival/5.bootstrap/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+var gulpTasks = require('./gulpfile.js');
+var gulp = require('gulp');
+
+app.use(express.static(__dirname + "/dist"));
+app.listen(3000);
+
+console.log("BOOTSTRAP app running in port 3000.");
+
+gulp.start('watch');
