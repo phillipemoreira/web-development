afc491cc6e98ff497ed4a6443788e352f8ab253b
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-03-09 00:15:59 -0300

    normalize port + use process.env.PORT

--- robpervival/5.bootstrap/index.js
+++ robpervival/5.bootstrap/index.js
@@ -3,9 +3,25 @@ var path = require('path');
 var gulpTasks = require('./gulpfile.js');
 var gulp = require('gulp');
 
+function normalizePort(val){
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+    }
+
+    return false;
+}
+
 var app = express();
 
 app.use(express.static(path.join(__dirname + "/dist")));
+
+var port = normalizePort(process.env.PORT || '3000');
 app.listen(3000);
 
 console.log("BOOTSTRAP app running in port 3000.");
