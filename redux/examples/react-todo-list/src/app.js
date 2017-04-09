c5320decc8722cf326897d82574ee1d250a3c75a
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-04-09 17:44:01 -0300

    Using ReactRedux provider

--- redux/examples/react-todo-list/src/app.js
+++ redux/examples/react-todo-list/src/app.js
@@ -6,23 +6,8 @@ const TodoApp = () => (
   </div>
 )
 
-class Provider extends Component {
-  getChildContext() {
-    return {
-      store: this.props.store
-    };
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-};
-
-const { createStore} = Redux;
+const { Provider } = ReactRedux;
+const { createStore } = Redux;
 
 ReactDOM.render(
   <Provider store={ createStore(todoAppReducers) }>
