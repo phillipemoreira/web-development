4d013233f3a245e0977cd6aa40e85d583eace729
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-06-03 17:13:14 -0300

    logo + rows

--- robpervival/5.bootstrap/js/landing-page.js
+++ robpervival/5.bootstrap/js/landing-page.js
@@ -1,1 +1,3 @@
-$('#top-container').css('height', $(window).height());
+$('#who').css('height', $(window).height() / 2.5 + 50);
+$('#products').css('height', $(window).height() / 2.5);
+$('#contact').css('height', $(window).height() / 2.5);
