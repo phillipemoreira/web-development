6bd3dc8d5d6d88e43345469ab06ff256d0d61df5
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-06-04 16:37:56 -0300

    Adding map

--- robpervival/5.bootstrap/js/landing-page.js
+++ robpervival/5.bootstrap/js/landing-page.js
@@ -1,1 +1,13 @@
-$('.page-section').css('min-height', $(window).height() / 1.5 - 50);
+$('.page-section').css('min-height', $(window).height() / 1.3 - 50);
+
+function initMap() {
+  var uluru = {lat: -23.444277, lng: -46.919568};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 19,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
