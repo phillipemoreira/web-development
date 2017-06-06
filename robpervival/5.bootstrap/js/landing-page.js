2b8ab022e71cfedaec9f465019f8ddd7e7927366
Phillipe Moreira <phillipealexmoreira@gmail.com>
2017-06-06 01:05:31 -0300

    finished footer

--- robpervival/5.bootstrap/js/landing-page.js
+++ robpervival/5.bootstrap/js/landing-page.js
@@ -1,9 +1,9 @@
-$('.page-section').css('min-height', $(window).height() / 1.3 - 50);
+$('.page-section').css('min-height', $(window).height() / 1.4 - 50);
 
 function initMap() {
   var uluru = {lat: -23.444277, lng: -46.919568};
   var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 19,
+    zoom: 17,
     center: uluru
   });
   var marker = new google.maps.Marker({
@@ -11,3 +11,9 @@ function initMap() {
     map: map
   });
 }
+
+
+$('#facebook-img').click(function() {
+    var profile = 'https://www.facebook.com/alzenia.moreira';
+    window.open(profile,'_blank','',''); 
+});
